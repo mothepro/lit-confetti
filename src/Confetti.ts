dfcd39d815c983d9e69766cea0c30c786978f2a4
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2019-12-16 18:35:43 -0800

    Pass colors into Confetti element

--- src/Confetti.ts
+++ src/Confetti.ts
@@ -10,9 +10,28 @@ export default class extends LitElement {
   @property({ type: Number })
   count = Math.floor((this.scrollHeight * this.scrollWidth) ** 0.33)
 
-  private particles = new Set<Particle>()
+  @property({ type: Boolean })
+  gradient = false
+
+  @property({ type: Array })
+  colors = [
+    { "red": 107, "green": 142, "blue": 35, },
+    { "red": 255, "green": 192, "blue": 20, },
+    { "red": 173, "green": 216, "blue": 23, },
+    { "red": 238, "green": 130, "blue": 23, },
+    { "red": 152, "green": 251, "blue": 15, },
+    { "red": 244, "green": 164, "blue": 96, },
+    { "red": 210, "green": 105, "blue": 30, },
+    { "red": 255, "green": 215, "blue": 0, },
+    { "red": 106, "green": 90, "blue": 20, },
+    { "red": 220, "green": 20, "blue": 60, },
+    { "red": 30, "green": 144, "blue": 25, },
+    { "red": 70, "green": 130, "blue": 18, },
+  ]
 
   private get canvas() { return this.shadowRoot?.getElementById('confetti') as HTMLCanvasElement }
+
+  private particles = new Set<Particle>()
 
   private context!: CanvasRenderingContext2D
 
@@ -35,34 +54,58 @@ export default class extends LitElement {
     for (const particle of this.particles) {
       particle.draw(this.context!)
       particle.update(this.speed)
-      if (!particle.visible)
+      if (!this.isVisible(particle))
         this.particles.delete(particle)
     }
 
     // Refill the particles set
     for (let i = this.particles.size; i < this.count; i++)
-      this.particles.add(new Particle(random(this.scrollWidth), this.scrollWidth, this.scrollHeight))
+      this.particles.add(new Particle(
+        random(this.scrollWidth),
+        -random(this.scrollHeight, 50),
+        this.getRandomStyle(random(1, 0.5))
+      ))
 
     Particle.waveAngle += 0.001
     if (this.particles.size)
       requestAnimationFrame(this.draw)
   }
 
-  firstUpdated() {
+  protected firstUpdated() {
     addEventListener('resize', this.resized)
     this.resized()
     this.context = this.canvas.getContext('2d')!
     requestAnimationFrame(this.draw)
   }
 
-  resized = () => {
+  protected resized = () => {
     this.canvas.width = this.scrollWidth
     this.canvas.height = this.scrollHeight
   }
 
-  updated(oldProps: Map<string, any>) {
+  protected updated(oldProps: Map<string, any>) {
     // Restart the rAF if we are now rendering particles again.
     if (!oldProps.get('count'))
       requestAnimationFrame(this.draw)
+  }
+
+  protected isVisible = (particle: Particle) =>
+    particle.y < this.scrollHeight + particle.radius
+    && particle.x > -particle.radius
+    && particle.x < this.scrollWidth + particle.radius
+
+  protected getRandomStyle(opacity: number): string | CanvasGradient {
+    if (this.gradient) {
+      const gradient = this.context.createLinearGradient(x, particle.y, x2, y2)
+      gradient.addColorStop(0, this.getRandomColor(opacity))
+      gradient.addColorStop(1, this.getRandomColor(opacity))
+      return gradient
+    } else
+      return this.getRandomColor(opacity)
+  }
+
+  protected getRandomColor(opacity: number) {
+    const { red, green, blue } = this.colors[Math.floor(random(this.colors.length))]
+    return `rgba(${red}, ${green}, ${blue}, ${opacity})`
   }
 }
--- src/Particle.ts
+++ src/Particle.ts
@@ -1,13 +1,5 @@
-import allColors from './allColors.json'
 import randomAngles from './randomAngles.js'
 import random from './random.js'
-
-interface Color {
-  red: number
-  green: number
-  blue: number
-  opacity?: number
-}
 
 export default class Particle {
   /** The general sway for all the current particles. */
@@ -15,23 +7,13 @@ export default class Particle {
 
   private static readonly maxTilt = 15
 
-  public y = random(this.radius * -2, -this.height)
-
   private tilt = 0
-
-  get visible() {
-    return this.y < this.height + this.radius
-      && this.x > -this.radius
-      && this.x < this.width + this.radius
-  }
 
   constructor(
     public x: number,
-    private readonly width: number,
-    private readonly height: number,
-    private readonly radius: number = random(20, 5),
-    private readonly opacity = random(1, 0.5),
-    private readonly color: Color = allColors[Math.floor(random(allColors.length))],
+    public y: number,
+    readonly style: string | CanvasGradient,
+    readonly radius: number = random(20, 5),
     private readonly angles = randomAngles(),
   ) { }
 
@@ -44,16 +26,9 @@ export default class Particle {
   draw(context: CanvasRenderingContext2D) {
     context.beginPath()
     context.lineWidth = this.radius * 2
-    context.strokeStyle = `rgba(${this.color.red}, ${this.color.green}, ${this.color.blue}, ${this.opacity})`
+    context.strokeStyle = this.style
     context.moveTo(this.x + this.tilt + this.radius, this.y)
     context.lineTo(this.x + this.tilt,               this.y + this.tilt + this.radius)
     context.stroke()
   }
-
-  /*
-      var gradient = context.createLinearGradient(x, particle.y, x2, y2);
-      gradient.addColorStop("0", particle.color);
-      gradient.addColorStop("1.0", particle.color2);
-      context.strokeStyle = gradient;
-    */
 }
